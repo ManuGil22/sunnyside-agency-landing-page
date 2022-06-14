d3802b2f4e54828ead0c7b0713a9b861877e5b60
Manuel Gil <manuelgilmoltrasio@gmail.com>
2022-06-14 15:15:42 -0300

    main section added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import mainImg from './images/desktop/image-header.jpg';
+import arrowIcon from './images/icon-arrow-down.svg';
 
 // Components
 import Header from './Components/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div className="main">
       <Header />
+      <img className="main-img" src={mainImg}></img>
+      <div className="heading-container">
+        <h1 className='main-title'>WE ARE CREATIVES</h1>
+        <img className='arrow-img' src={arrowIcon}></img>
+      </div>
     </div>
   );
 }
