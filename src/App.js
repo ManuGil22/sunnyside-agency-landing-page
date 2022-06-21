9905c2b6e1a0bda5e823516af4622ee41eaeab6f
Manuel Gil <manuelgilmoltrasio@gmail.com>
2022-06-21 10:06:45 -0300

    Fixed some ImageCard responsive bugs and sizes

--- src/App.js
+++ src/App.js
@@ -33,11 +33,13 @@ function App() {
           text="Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places." 
           color="red" />
         <ImageCard 
+          className="graphic"
           imgSrc={graphicDesingImg} 
           title="Graphic design" 
           text=" Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention."
           color="green" />
         <ImageCard 
+          className="photography"
           imgSrc={photographyImg}
           title="Photography"
           text="Increase your credibility by getting the most stunning, high-quality photos that improve your business image."
--- src/Components/ImageCard.js
+++ src/Components/ImageCard.js
@@ -1,12 +1,14 @@
 import '../Stylesheets/ImageCard.css';
 
-function ImageCard({ title, text, imgSrc, color }){
+function ImageCard({ title, text, imgSrc, color, className }){
     return(
         <div className="img-container grid-item" >
-            <img className='image' src={imgSrc}></img>
-            <div className={`img-info ${color}`}>
-                <h1 className={`img-title ${title}`}>{title}</h1>
-                <p className={`img-text ${text}`}>{text}</p>
+            <img className={`image ${className ? className : ''}`} src={imgSrc}></img>
+            <div className='relative-container'>
+                <div className={`img-info ${color}`}>
+                    <h1 className={`img-title ${title}`}>{title}</h1>
+                    <p className={`img-text ${text}`}>{text}</p>
+                </div>
             </div>
         </div>
     )
